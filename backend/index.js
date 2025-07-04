a1e241f95390ffbc18df20e2ed2fe9ffeccfeb6b
ankitjhajhariya <ankitjhajhariya018@gmail.com>
2025-07-04 16:14:09 +0530

    y

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './backend/.env' });
+dotenv.config();
 
 const authRoutes = require('./routes/Auth.router');
 
